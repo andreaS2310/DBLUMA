4d79b3e004c33012b2aa1811d975ded5ecf06a50
andreaS2310 <suloandrea69@gmail.com>
2025-09-26 21:44:31 +0200

    Update commands.cmd

--- web-source/commands.cmd
+++ web-source/commands.cmd
@@ -7,3 +7,8 @@ pip install streamlit pandas
 streamlit run t.py
 
 ::#region fine print via traction
+Video Preview:
+<video controls width="640">
+  <source src="media/preview.webm" type="video/webm">
+  Your browser does not support the video tag. Please go to the '/media' folder and watch it there.
+</video>
